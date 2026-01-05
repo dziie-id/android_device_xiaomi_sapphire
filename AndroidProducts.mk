471a9dbcc8c70f92f11566d91bb88a3f981adf8a
Nicooo-Afk <dzakialfariz174@gmail.com>
2026-01-05 09:54:23 +0000

    Revert "sapphire: InfinityX bringup"
    This reverts commit 3efabc3579800360e90426dfdbe8f17cb5ff2d23.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_sapphire.mk
+    $(LOCAL_DIR)/lineage_sapphire.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sapphire-user \
+    lineage_sapphire-userdebug \
+    lineage_sapphire-eng
--- lineage_sapphire.mk
+++ lineage_sapphire.mk
@@ -12,19 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sapphire/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Include our private certificate
 -include vendor/lineage-priv/keys/keys.mk
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Build flags
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := "Lieko"
-TARGET_HAS_UDFPS := true
-WITH_GAPPS := true
-TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := lineage_sapphire
 PRODUCT_DEVICE := sapphire
